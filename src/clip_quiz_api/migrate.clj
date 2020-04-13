8021d178b5d89f3950421d71dc4fb415a359a75d
Tyler Kindy <tylerkindy@gmail.com>
2020-04-13 09:51:15 -0400

    Extract database URL to env

--- src/clip_quiz_api/migrate.clj
+++ src/clip_quiz_api/migrate.clj
@@ -1,13 +1,12 @@
 (ns clip-quiz-api.migrate
   (:require [clj-liquibase.cli :as cli]
             [clj-liquibase.core :refer [defparser]]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [dotenv :refer [env]]))
 
 (defparser app-changelog "changelog.sql")
 
-(def ds (jdbc/get-datasource
-         {:dbtype "mysql" :host "172.17.0.1" :dbname "clip-quiz"
-          :user "root" :password "mysecretpw"}))
+(def ds (jdbc/get-datasource (env :MIGRATION_DATABASE_URL)))
 
 (defn -main [& [cmd & args]]
   (apply cli/entry cmd {:datasource ds :changelog app-changelog}
