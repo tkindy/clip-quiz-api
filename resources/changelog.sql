809ccbde6ad90e8432e98c35a93808f9f0519517
Tyler Kindy <tylerkindy@gmail.com>
2020-04-19 13:09:49 -0400

    Add basic database schema

--- resources/changelog.sql
+++ resources/changelog.sql
@@ -5,3 +5,20 @@ CREATE TABLE "players" (
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(16) NOT NULL
 );
+
+--changeset tkindy:2-add-rooms
+CREATE TABLE "rooms" (
+  "id" SERIAL PRIMARY KEY,
+  "passphrase" VARCHAR(16) NOT NULL,
+  "playlistId" VARCHAR(32) NOT NULL,
+  "leaderId" INTEGER NOT NULL REFERENCES "players"("id")
+);
+
+--changeset tkindy:3-add-room-players
+CREATE TABLE "roomPlayers" (
+  "playerId" INTEGER REFERENCES "players"("id") PRIMARY KEY,
+  "roomId"  INTEGER NOT NULL REFERENCES "rooms"("id")
+);
+
+--changeset tkindy:4-add-room-players-index
+CREATE INDEX "roomPlayers_roomId" ON "roomPlayers" ("roomId");
