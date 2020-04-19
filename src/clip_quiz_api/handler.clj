ff3a798004ad119a772a2fc661a93e990319e611
Tyler Kindy <tylerkindy@gmail.com>
2020-04-19 15:07:41 -0400

    Use separate Spotify routes, add cookie middleware

--- src/clip_quiz_api/handler.clj
+++ src/clip_quiz_api/handler.clj
@@ -1,9 +1,11 @@
 (ns clip-quiz-api.handler
-  (:require [compojure.core :refer [defroutes routes context GET]]
+  (:require [compojure.core :refer [defroutes context GET]]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.cookies :refer [wrap-cookies]]
             [clip-quiz-api.app :refer [get-tables]]
+            [clip-quiz-api.routes.spotify :refer [spotify-routes]]
             [org.httpkit.server :refer [with-channel on-close on-receive send!]]))
 
 (def clients (atom {}))
@@ -17,15 +19,11 @@
                      (swap! clients dissoc conn)
                      (println conn "disconnected. status:" status)))))
 
-(defn spotify-routes []
-  (routes
-   ()))
-
 (defroutes app-routes
   (GET "/" {app ::app} {:body (map :tables/table_name (get-tables app))})
   (GET "/obj" [] {:body {:name "Tyler" :age 24 :hungry true :aliases ["Gene" "Ted"]}})
   (GET "/push" [] ws)
-  (context "/spotify" [] (spotify-routes))
+  (context "/spotify" [] spotify-routes)
   (route/not-found "Not Found"))
 
 (defn wrap-app-component [f app]
@@ -41,5 +39,6 @@
 
   (-> app-routes
       (wrap-app-component app)
-      (wrap-json-response)
+      wrap-json-response
+      wrap-cookies
       (wrap-defaults site-defaults)))
